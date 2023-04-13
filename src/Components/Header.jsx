25b8b7be02d411a2c26c0f37cd62d56c52b9043c
carullahtursun <62027425+carullahtursun@users.noreply.github.com>
2023-04-13 17:48:00 +0300

    Google book search app icons chenged

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { AiOutlineGithub, AiFillLinkedin, AiOutlineLink } from 'react-icons/Ai';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import AddLinkIcon from '@mui/icons-material/AddLink';
 function Header() {
     return (
         <div className='bg-teal-500 p-5 text-white  fixed bottom-0 w-full mt-12' >
             <div className='flex flex-row w-full justify-center items-center space-x-8 '>
                 <a href="https://images.pexels.com/photos/12064/pexels-photo-12064.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
-                    < AiOutlineGithub className='h-6 w-6' />
+                    < GitHubIcon className='h-6 w-6' />
                 </a>
                 <a href="https://www.linkedin.com/in/carullahtursun/">
-                    < AiFillLinkedin className='h-6 w-6' />
+                    < LinkedInIcon className='h-6 w-6' />
                 </a>
                 <a href="https://carullahtursun.vercel.app">
-                    < AiOutlineLink className='h-6 w-6' />
+                    < AddLinkIcon className='h-6 w-6' />
                 </a>
             </div>
         </div>
